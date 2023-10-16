2c2ebde1a7156867673162a34b93976cce1d050c
quoc huy <nguyenquochuy3102@gmail.com>
2023-10-16 14:39:25 +0700

    them file test.cpp

---
+++ test.cpp
@@ -0,0 +1,24 @@
+////2051052058_NguyenQuocHuy_BT03
+//#include <iostream>
+//#include <string>
+//#include <iomanip>
+//using namespace std;
+//int main()
+//{ 
+// string ten;
+// int soNgay;
+// double tienCong;
+// double tienLuong;
+// cout <<"nhap ten: ";
+// getline ( cin, ten );
+// cout << "nhap so ngay lam viec: ";
+// cin >> soNgay;
+// cout <<" nhap tien cong 1 ngay: ";
+// cin >>tienCong;
+// tienLuong = soNgay * tienCong;
+// cout <<"Ten:" << ten << endl;
+// cout <<" tien luong:" << fixed << setprecision (0) 
+//	 << tienLuong
+// system ("pause");
+// return 0;
+//}
